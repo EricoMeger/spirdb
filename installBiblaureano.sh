ce53cf948c7a5f66508c9bb5efe8d80bcf9b0dce
Érico Meger <86668447+EricoMeger@users.noreply.github.com>
2023-02-05 16:59:13 -0300

    Add files via upload

---
+++ installBiblaureano.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt install unzip
+
+echo "baixando biblaureano"
+wget -O biblaureano_3.9.zip 'https://downloads.sourceforge.net/project/biblaureano/biblaureano_3.9.zip?ts=gAAAAABj37h19n2jR88dXPNIlBNDKYIh-u_mLkjh5quO__8-Y1QsA0xv7NPGxAbSPxNR2ab48jLG4DB9qM0-GQ0OZloSOjgIXg==&use_mirror=master&r=https://sourceforge.net/projects/biblaureano/'
+
+echo "criando pasta para a biblioteca"
+mkdir -p ../Documents/biblaureano
+mv biblaureano_3.9.zip ../Documents/biblaureano
+cp installUBT2204.sh ../Documents/biblaureano
+cd ../Documents/biblaureano
+
+unzip biblaureano_3.9.zip
+echo "biblaureano unzipada"
+
+echo "movendo script de instalacao para a pasta"
+mv installUBT2204.sh Biblaureano/
+cd Biblaureano/
+
+echo "dando permissão de execução ao script de instalação"
+sudo chmod +x installUBT2204.sh
+
+echo "executando script de instalação"
+sh installUBT2204.sh
+
+
+
---
+++ installUBT2204.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+##########################
+# chmod +x install.sh
+# ./install.sh
+##########################
+
+#Esse arquivo é o install.sh feito originalmente pelos autores da Biblaureano, onde eu modifiquei o diretório de alguns arquivos para que o script pudesse funcionar na versão 22.04 do Ubuntu. Todos os créditos à seus autores.
+
+echo "Removendo arquivos antigos"
+rm ./obj/Debug/*
+rm ./bin/Debug/*
+
+echo "Compilando a biblioteca"
+g++ -w -Wall -fPIC -Wsign-compare -fexceptions -g -I "./" -c "./main.cpp" -o "./obj/Debug/main.o"
+g++ -shared  "./obj/Debug/main.o"   -o "./bin/Debug/Biblaureano.so"
+
+echo "Copiando arquivos para os diretórios do sistema"
+
+sudo cp ./bin/Debug/Biblaureano.so /lib
+
+#Copia o arquivo de configuração com a biblioteca já linkada
+sudo cp default.conf ~/.config/codeblocks/
+
+#Copia o .h para o diretorio c++
+sudo cp ./biblaureano.h /usr/include/c++/
+#Joga o .h pra todas as pastas dentro da c++ e remove o .h da c++
+cd /usr/include/c++/
+for i in *; do
+	sudo cp ./biblaureano.h $i 2> /dev/null
+done
+sudo rm biblaureano.h
+
+echo "Terminado!"
+
