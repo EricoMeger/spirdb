bc983c636b1f7d913b3dcd420aeb832c58893ebf
Érico Meger <86668447+EricoMeger@users.noreply.github.com>
2023-02-17 08:12:59 -0300

    Update installBiblaureano.sh

--- installBiblaureano.sh
+++ installBiblaureano.sh
@@ -8,10 +8,10 @@ echo "baixando biblaureano"
 wget -O biblaureano_3.9.zip 'https://downloads.sourceforge.net/project/biblaureano/biblaureano_3.9.zip?ts=gAAAAABj37h19n2jR88dXPNIlBNDKYIh-u_mLkjh5quO__8-Y1QsA0xv7NPGxAbSPxNR2ab48jLG4DB9qM0-GQ0OZloSOjgIXg==&use_mirror=master&r=https://sourceforge.net/projects/biblaureano/'
 
 echo "criando pasta para a biblioteca"
-mkdir -p ../Documents/biblaureano
-mv biblaureano_3.9.zip ../Documents/biblaureano
-cp installUBT2204.sh ../Documents/biblaureano
-cd ../Documents/biblaureano
+mkdir ~/Documents/biblaureano
+mv biblaureano_3.9.zip ~/Documents/biblaureano
+cp i~nstallUBT2204.sh ~/Documents/biblaureano
+cd ~/Documents/biblaureano
 
 unzip biblaureano_3.9.zip
 echo "biblaureano unzipada"
